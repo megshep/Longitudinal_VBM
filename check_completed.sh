c8e39b922ba934efaacb933b11d2e5a6e85b48d4
Meg Sheppard <142318637+megshep@users.noreply.github.com>
2025-09-28 19:58:06 +0100

    Update check_completed.sh
    editing the comments of the code to be more detailed and specific

--- check_completed.sh
+++ check_completed.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# Directory containing your SLURM output files
+# Directory containing your output and error files from each of the job arrays submitted (logs)
 LOGDIR=/mnt/iusers01/nm01/j90161ms/logs
 
-# Count total submitted jobs
+# Counts the total jobs that were submitted
 TOTAL=$(ls $LOGDIR/serial_reg_6855517_*.out 2>/dev/null | wc -l)
 
-# Count completed jobs (look for the specific "Done" message)
+# Counts the total completed jobs (look for the specific "Done" message that is produced in a successful .out file)
 DONE_COUNT=$(grep -l "Done" $LOGDIR/serial_reg_6855517_*.out 2>/dev/null | wc -l)
 
-# Print summary
+# Print summary to tell us how many were run and how many of those were successful
 echo "Total jobs submitted: $TOTAL"
 echo "Jobs completed: $DONE_COUNT"
