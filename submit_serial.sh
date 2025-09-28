a05c3d76f5c2a991ee0eb8787d0e95c18ed306f5
Meg Sheppard <142318637+megshep@users.noreply.github.com>
2025-09-28 18:56:12 +0100

    Update submit_serial.sh
    This code is edited as I ended up using SPM25 instead of SPM12

--- submit_serial.sh
+++ submit_serial.sh
@@ -2,19 +2,16 @@
 #SBATCH --job-name=serial_reg
 #SBATCH --output=/mnt/iusers01/nm01/j90161ms/logs/serial_reg_%A_%a.out
 #SBATCH --error=/mnt/iusers01/nm01/j90161ms/logs/serial_reg_%A_%a.err
-#SBATCH --array=1-1233%20
+#SBATCH --array=1-1233%1233
 #SBATCH --time=72:00:00
-#SBATCH --mem=8G
+#SBATCH --mem=20G
 #SBATCH --cpus-per-task=1
 
 # Load required modules
 module load apps/binapps/matlab/R2024b 
-module load apps/binapps/matlab/third-party-toolboxes/spm/12.5
 
-source $SPM_COPY
-
-# Set the SPM path environment variable (assuming you've copied SPM with source $SPM_COPY)
-export USER_SPM_DIR="/mnt/iusers01/nm01/j90161ms/scratch/SPM12.5"
+# Set the SPM path environment variable
+export USER_SPM_DIR="/mnt/iusers01/nm01/j90161ms/scratch/spm25/spm"
 
 # Ensure we run in the job submission directory
 cd "$SLURM_SUBMIT_DIR"
@@ -23,3 +20,6 @@ SUB_ID=$SLURM_ARRAY_TASK_ID
 
 # Run the MATLAB script 
 matlab -nodisplay -nosplash -batch "try;addpath(getenv('USER_SPM_DIR'));run('/mnt/iusers01/nm01/j90161ms/~longitudinal_vbm/serial_reg_csf.m');catch ME;fprintf(2,'Error: %s\n', ME.message);disp(getReport(ME, 'extended'));end;exit"
+
+
+
