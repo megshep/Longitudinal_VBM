882b9a3de02ddf1bc84c2d31f1a03d8d7d3d50a8
Meg Sheppard <142318637+megshep@users.noreply.github.com>
2025-09-28 20:13:09 +0100

    Update submit_serial.sh
    more detailed in-code comments

--- submit_serial.sh
+++ submit_serial.sh
@@ -1,3 +1,4 @@
+%%defines the parameters for CSF3, outlining the name of the job, locations of logs, array information, times, memory for the job and CPUs
 #!/bin/bash
 #SBATCH --job-name=serial_reg
 #SBATCH --output=/mnt/iusers01/nm01/j90161ms/logs/serial_reg_%A_%a.out
@@ -7,10 +8,10 @@
 #SBATCH --mem=20G
 #SBATCH --cpus-per-task=1
 
-# Load required modules
+# Load in MATLAB 
 module load apps/binapps/matlab/R2024b 
 
-# Set the SPM path environment variable
+# Set the SPM path environment variable - this includes the directory where SPM itself is saved
 export USER_SPM_DIR="/mnt/iusers01/nm01/j90161ms/scratch/spm25/spm"
 
 # Ensure we run in the job submission directory
