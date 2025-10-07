95e5a526ff95d02ef95b03a474304da578277ac1
Meg Sheppard <142318637+megshep@users.noreply.github.com>
2025-10-07 20:49:18 +0100

    Create segment_arrays_883_1233.m
    This is an edited version of segmentation MATLAB file - this splits it into job arrays from file 883 to 1233 (my final file). This is because the job was terminated at 883 so I needed to run the final few as job arrays to make up for lost time!

---
+++ segment_arrays_883_1233.m
@@ -0,0 +1,57 @@
+%% Get SLURM job array index
+SUB_ID = str2double(getenv('SLURM_ARRAY_TASK_ID'));
+
+%% Automatically find all avg_*.nii files
+all_files = dir('/scratch/j90161ms/avg_*.nii');
+all_files = fullfile({all_files.folder}, {all_files.name});
+all_files = sort(all_files);  % Ensure consistent order
+
+%% Keep only files 883 → 1233
+all_files = all_files(883:end);
+
+%% Select the file corresponding to this SLURM array job
+this_file = all_files{SUB_ID};
+matlabbatch{1}.spm.spatial.preproc.channel.vols = {this_file};
+
+%% batch
+matlabbatch{1}.spm.spatial.preproc.channel.biasreg = 0.001;
+matlabbatch{1}.spm.spatial.preproc.channel.biasfwhm = 60;
+matlabbatch{1}.spm.spatial.preproc.channel.write = [0 0];
+matlabbatch{1}.spm.spatial.preproc.tissue(1).tpm = {'/scratch/j90161ms/spm25/spm/tpm/TPM.nii,1'};
+matlabbatch{1}.spm.spatial.preproc.tissue(1).ngaus = 1;
+matlabbatch{1}.spm.spatial.preproc.tissue(1).native = [1 1];
+matlabbatch{1}.spm.spatial.preproc.tissue(1).warped = [0 0];
+matlabbatch{1}.spm.spatial.preproc.tissue(2).tpm = {'/scratch/j90161ms/spm25/spm/tpm/TPM.nii,2'};
+matlabbatch{1}.spm.spatial.preproc.tissue(2).ngaus = 1;
+matlabbatch{1}.spm.spatial.preproc.tissue(2).native = [1 1];
+matlabbatch{1}.spm.spatial.preproc.tissue(2).warped = [0 0];
+matlabbatch{1}.spm.spatial.preproc.tissue(3).tpm = {'/scratch/j90161ms/spm25/spm/tpm/TPM.nii,3'};
+matlabbatch{1}.spm.spatial.preproc.tissue(3).ngaus = 2;
+matlabbatch{1}.spm.spatial.preproc.tissue(3).native = [1 0];
+matlabbatch{1}.spm.spatial.preproc.tissue(3).warped = [0 0];
+matlabbatch{1}.spm.spatial.preproc.tissue(4).tpm = {'/scratch/j90161ms/spm25/spm/tpm/TPM.nii,4'};
+matlabbatch{1}.spm.spatial.preproc.tissue(4).ngaus = 3;
+matlabbatch{1}.spm.spatial.preproc.tissue(4).native = [0 0];
+matlabbatch{1}.spm.spatial.preproc.tissue(4).warped = [0 0];
+matlabbatch{1}.spm.spatial.preproc.tissue(5).tpm = {'/scratch/j90161ms/spm25/spm/tpm/TPM.nii,5'};
+matlabbatch{1}.spm.spatial.preproc.tissue(5).ngaus = 4;
+matlabbatch{1}.spm.spatial.preproc.tissue(5).native = [0 0];
+matlabbatch{1}.spm.spatial.preproc.tissue(5).warped = [0 0];
+matlabbatch{1}.spm.spatial.preproc.tissue(6).tpm = {'/scratch/j90161ms/spm25/spm/tpm/TPM.nii,6'};
+matlabbatch{1}.spm.spatial.preproc.tissue(6).ngaus = 2;
+matlabbatch{1}.spm.spatial.preproc.tissue(6).native = [0 0];
+matlabbatch{1}.spm.spatial.preproc.tissue(6).warped = [0 0];
+matlabbatch{1}.spm.spatial.preproc.warp.mrf = 1;
+matlabbatch{1}.spm.spatial.preproc.warp.cleanup = 1;
+matlabbatch{1}.spm.spatial.preproc.warp.reg = [0 0.001 0.5 0.05 0.2];
+matlabbatch{1}.spm.spatial.preproc.warp.affreg = 'mni';
+matlabbatch{1}.spm.spatial.preproc.warp.fwhm = 0;
+matlabbatch{1}.spm.spatial.preproc.warp.samp = 3;
+matlabbatch{1}.spm.spatial.preproc.warp.write = [0 0];
+matlabbatch{1}.spm.spatial.preproc.warp.vox = NaN;
+matlabbatch{1}.spm.spatial.preproc.warp.bb = [NaN NaN NaN
+                                              NaN NaN NaN];
+
+%% Run the segmentation
+spm_jobman('run', matlabbatch);
+
