9849fb8b35d89c12508a7d519428c4da560070dc
Meg Sheppard <142318637+megshep@users.noreply.github.com>
2025-10-07 19:50:03 +0100

    Update segment_avg.sh
    Adds in the information for the job arrays. The CSF terminated the job after 883 completed so the arrays take this as the starting point

--- segment_avg.sh
+++ segment_avg.sh
@@ -1,8 +1,8 @@
-!/bin/bash
+#!/bin/bash
 #SBATCH --job-name=segment_avg
-#SBATCH --output=/mnt/iusers01/nm01/j90161ms/logs/segment_avg_%A_%a.out
-#SBATCH --error=/mnt/iusers01/nm01/j90161ms/logs/segment_avg_%A_%a.err
-#SBATCH --array=1-1231%1231
+#SBATCH --output=/mnt/iusers01/nm01/j90161ms/logs/segment_logs/segment_avg_%A_%a.out
+#SBATCH --error=/mnt/iusers01/nm01/j90161ms/logs/segment_logs/segment_avg_%A_%a.err
+#SBATCH --array=883-1231%%348
 #SBATCH --time=72:00:00
 #SBATCH --mem=20G
 #SBATCH --cpus-per-task=1
@@ -16,8 +16,11 @@ export USER_SPM_DIR="/mnt/iusers01/nm01/j90161ms/scratch/spm25/spm"
 # Ensure we run in the job submission directory
 cd "$SLURM_SUBMIT_DIR"
 
-SUB_ID=$SLURM_ARRAY_TASK_ID
+# Assign the SLURM array task ID to a variable that can be inputted into the matlab script below
+export SUB_ID=$SLURM_ARRAY_TASK_ID
 
 # Run the MATLAB script 
-matlab -nodisplay -nosplash -batch "try;addpath(getenv('USER_SPM_DIR'));run('/mnt/iusers01/nm01/j90161ms/~longitudi>
+matlab -nodisplay -nosplash -batch "try;addpath(getenv('USER_SPM_DIR'));run('/mnt/iusers01/nm01/j90161ms/~longitudinal_vbm/segment.m');catch ME;fprintf(2,'Error: %s\n', ME.message);disp(getReport(ME, 'extended'));end;exit"
+
+
 
