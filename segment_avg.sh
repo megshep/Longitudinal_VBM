46ce383db5d32b4f7b4dba156c04767614b60e15
Meg Sheppard <142318637+megshep@users.noreply.github.com>
2025-10-07 20:50:36 +0100

    Update segment_avg.sh
    The final shell script used for the full segmentation job (non-job array) on UoM CSF3

--- segment_avg.sh
+++ segment_avg.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=segment_avg
-#SBATCH --output=/mnt/iusers01/nm01/j90161ms/logs/segment_logs/segment_avg_%A_%a.out
-#SBATCH --error=/mnt/iusers01/nm01/j90161ms/logs/segment_logs/segment_avg_%A_%a.err
-#SBATCH --array=883-1231%%348
+#SBATCH --output=/mnt/iusers01/nm01/j90161ms/logs/segment_logs/segment_avg_%j.out
+#SBATCH --error=/mnt/iusers01/nm01/j90161ms/logs/segment_logs/segment_avg_%j.err
 #SBATCH --time=72:00:00
 #SBATCH --mem=20G
 #SBATCH --cpus-per-task=1
@@ -15,9 +14,6 @@ export USER_SPM_DIR="/mnt/iusers01/nm01/j90161ms/scratch/spm25/spm"
 
 # Ensure we run in the job submission directory
 cd "$SLURM_SUBMIT_DIR"
-
-# Assign the SLURM array task ID to a variable that can be inputted into the matlab script below
-export SUB_ID=$SLURM_ARRAY_TASK_ID
 
 # Run the MATLAB script 
 matlab -nodisplay -nosplash -batch "try;addpath(getenv('USER_SPM_DIR'));run('/mnt/iusers01/nm01/j90161ms/~longitudinal_vbm/segment.m');catch ME;fprintf(2,'Error: %s\n', ME.message);disp(getReport(ME, 'extended'));end;exit"
